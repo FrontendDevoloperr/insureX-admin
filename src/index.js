bcb5cfef2c1971417a82c87fd3215c2483e37861
100798f <farhodbek.nomonov@mail.ru>
2022-07-17 17:39:45 +0500

    build(deps): update package-lock.json, package.json and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import io from "socket.io-client";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+
+const socket = io("https://api.insurextest.link", { reconnect: true });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
