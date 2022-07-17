9572045255ad1e07eed1f5b39f8c8b2035229278
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2022-07-17 17:10:23 +0500

    update src/utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const _URL = "http://3.91.9.208:3002/api";
+export const _URL = "https://api.insurextest.link/api";
 
 export const GetRegionName = async (regionId) => {
   if (regionId === false) return false;
