1f30f3a24a71413ff3b103b19c24ac4a8b109547
100798f <farhodbek.nomonov@mail.ru>
2022-07-18 11:32:39 +0500

    update popup.js

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -1,59 +1,38 @@
 import { useState } from "react";
-import { Popper, Button, Paper, Group, useMantineTheme } from "@mantine/core";
+import { Popover, Text } from "@mantine/core";
 import { Notification } from "../icons";
-import "../admin/index.css";
 
 function Popup() {
-  const [referenceElement, setReferenceElement] = useState(null);
-  const [visible, setVisible] = useState(false);
-  const theme = useMantineTheme();
-
+  const [opened, setOpened] = useState(false);
   return (
-    <div>
-      <Button
-        className="notification_icon"
-        ref={setReferenceElement}
-        onClick={() => setVisible((m) => !m)}
-      >
-        <Notification />
-      </Button>
-
-      <Popper
-        placement="end"
-        arrowSize={5}
-        withArrow
-        mounted={visible}
-        referenceElement={referenceElement}
-        transition="pop-bottom-left"
-        transitionDuration={200}
-        arrowStyle={{
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[5]
-              : theme.colors.gray[1],
+    <Popover
+      opened={opened}
+      onClose={() => setOpened(false)}
+      target={
+        <Notification
+          style={{ cursor: "pointer" }}
+          onClick={() => setOpened((o) => !o)}
+        />
+      }
+      width={200}
+      position="bottom"
+      withArrow
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center",
         }}
       >
-        <Paper
-          style={{
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[5]
-                : theme.colors.gray[1],
-          }}
-        >
-          {[0, 1, 2, 3, 4, 5].map((item) => (
-            <p
-              style={{
-                padding: "0.5rem",
-              }}
-              key={item}
-            >
-              Notification
-            </p>
-          ))}
-        </Paper>
-      </Popper>
-    </div>
+        <Text size="sm">Notifications</Text>
+        <Text size="sm">Notifications</Text>
+        <Text size="sm">Notifications</Text>
+        <Text size="sm">Notifications</Text>
+        <Text size="sm">Notifications</Text>
+      </div>
+    </Popover>
   );
 }
 export default Popup;
