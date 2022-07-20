fbe661658504abd6b0d6584d641621cfdebdd26d
100798f <farhodbek.nomonov@mail.ru>
2022-07-20 15:33:51 +0500

    update popup.js

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -92,8 +92,9 @@ function Popup() {
               </Box>
             )}
 
-            {__message?.reverse()?.map((message) => (
+            {__message?.reverse()?.map((message, i) => (
               <Box
+                style={i >= 10 ? { display: "none" } : {}}
                 title={message?.role}
                 onClick={(e) => {
                   e.preventDefault();
