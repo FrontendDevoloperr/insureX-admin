cbce0d40e92d3f319ce21539a2e678c5584b2d21
100798f <farhodbek.nomonov@mail.ru>
2022-09-05 16:48:24 +0500

    update App.js and popup.js

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -27,55 +27,36 @@ function Popup() {
   React.useEffect(() => {
     if (user?.auth) {
       socket.on("message-send", (msg) => {
-        console.log(msg);
-        if (user.role === "superadmin") {
-          setNewMessage(true);
+        if (msg.admin_type) {
+          console.log(msg);
+          if (user.role === "superadmin") {
+            setMessages([...messages, msg]);
+            setNewMessage(true);
+            toast.success("New message received");
+          }
+          if (
+            user.role === "insurance_company" &&
+            Number(user.insurance_company.id) ===
+              Number(msg.insurance_company_id)
+          ) {
+            setMessages([...messages, msg]);
+            setNewMessage(true);
+            toast.success("New message received");
+          }
+          if (
+            user.role === "appraisal_company" &&
+            Number(user.appraisal_company.id) ===
+              Number(msg.appraisal_company_id)
+          ) {
+            setMessages([...messages, msg]);
+            setNewMessage(true);
+            toast.success("New message received");
+          }
         }
-        if (
-          user.role === "insurance_company" &&
-          Number(user.insurance_company.id) === Number(msg.insurance_company_id)
-        ) {
-          setNewMessage(true);
-          toast.success("New message received");
-        }
-        if (
-          user.role === "appraisal_company" &&
-          Number(user.appraisal_company.id) === Number(msg.appraisal_company_id)
-        ) {
-          setNewMessage(true);
-          toast.success("New message received");
-        }
-        axios
-          .get(`${_URL}/push/messages`, {
-            headers: {
-              Authorization: `Bearer ${
-                JSON.parse(localStorage.getItem("admin-panel-token-insure-x"))
-                  .token
-              } `,
-            },
-          })
-          .then((res) => {
-            setMessages(
-              res?.data?.messages?.filter((__message) =>
-                user.role === "superadmin"
-                  ? __message?.role ||
-                    __message?.ie_number ||
-                    __message?.oao_ie_number
-                  : user.role === "insurance_company"
-                  ? __message?.ie_number ||
-                    Number(__message?.insurance_company_id) ===
-                      Number(user.insurance_company.id)
-                  : user.role === "appraisal_company"
-                  ? __message?.oao_ie_number ||
-                    Number(__message?.appraisal_company_id) ===
-                      Number(user.appraisal_company.id)
-                  : false
-              )
-            );
-          });
       });
     }
   }, [user?.auth]);
+
   React.useEffect(() => {
     if (user?.auth) {
       axios
@@ -87,27 +68,31 @@ function Popup() {
             } `,
           },
         })
-        .then((res) => {
-          setMessages(
-            res?.data?.messages?.filter((__message) =>
-              user.role === "superadmin"
-                ? __message?.role ||
-                  __message?.ie_number ||
-                  __message?.oao_ie_number
-                : user.role === "insurance_company"
-                ? __message?.ie_number ||
-                  Number(__message?.insurance_company_id) ===
-                    Number(user.insurance_company.id)
-                : user.role === "appraisal_company"
-                ? __message?.oao_ie_number ||
-                  Number(__message?.appraisal_company_id) ===
-                    Number(user.appraisal_company.id)
-                : false
-            )
-          );
+        .then(({ data }) => {
+          if (user?.role === "superadmin") {
+            setMessages(data?.messages?.filter((res) => res.admin_type));
+          }
+          if (user?.role === "insurance_company") {
+            setMessages(
+              data?.filter(
+                (res) =>
+                  Number(res?.insurance_company_id) ===
+                    Number(user?.insurance_company?.id) && res?.admin_type
+              )
+            );
+          }
+          if (user?.role === "appraisal_company") {
+            setMessages(
+              data?.filter(
+                (res) =>
+                  Number(res?.appraisal_company_id) ===
+                    Number(user?.appraisal_company?.id) && res?.admin_type
+              )
+            );
+          }
         });
     }
-  }, [user?.auth]);
+  }, [user?.auth, user?.role]);
 
   return (
     <Popover
@@ -153,7 +138,6 @@ function Popup() {
                   borderRadius: theme.radius.md,
                   marginBottom: theme.spacing.sm,
                   cursor: "pointer",
-
                   "&:hover": {
                     backgroundColor:
                       theme.colorScheme === "dark"
@@ -170,88 +154,75 @@ function Popup() {
             )}
 
             <div style={{ display: "flex", flexDirection: "column-reverse" }}>
-              {messages
-                ?.filter((_res) => _res.role !== "customer")
-                .map((message, i) => (
-                  <React.Fragment key={message?.id + Math.random()}>
-                    <Box
-                      title={message?.role}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        if (message?.role === "insured_person") {
-                          navigate(`/persons`);
-                          setOpened(false);
-                        }
-                        if (message?.role === "sdp") {
-                          navigate(`/sdp`);
-                          setOpened(false);
-                        }
-                        if (message?.role === "agent") {
-                          navigate(`/agents`);
-                          setOpened(false);
-                        }
-                        if (message?.role === "appraiser") {
-                          navigate(`/appraisers`);
-                          setOpened(false);
-                        }
-                        if (message?.oao_ie_number) {
-                          navigate(`/appraiser-company`);
-                          setOpened(false);
-                        }
-                        if (message?.ie_number) {
-                          navigate(`/insurance-company`);
-                          setOpened(false);
-                        }
-                        if (
-                          message?.appraisal_company_id &&
-                          !message?.first_name
-                        ) {
-                          navigate(`/events`);
-                          setOpened(false);
-                        }
-                      }}
-                      sx={(theme) => ({
+              {messages?.map((message, i) => (
+                <React.Fragment key={message?.id + i}>
+                  <Box
+                    title={message?.user_type}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      if (message?.user_type === "persons") {
+                        navigate(`/persons#${message?.user_id ?? "users"}`);
+                        setOpened(false);
+                      }
+                      if (message?.user_type === "sdp") {
+                        navigate(`/sdp`);
+                        setOpened(false);
+                      }
+                      if (message?.user_type === "agent") {
+                        navigate(`/agents`);
+                        setOpened(false);
+                      }
+                      if (message?.user_type === "appraiser") {
+                        navigate(`/appraisers`);
+                        setOpened(false);
+                      }
+                      if (
+                        message?.user_type ===
+                        ("appraisal_company" || "event_create")
+                      ) {
+                        navigate(`/appraiser-company`);
+                        setOpened(false);
+                      }
+                      if (
+                        message?.user_type ===
+                        ("insurance_company" || "event_create")
+                      ) {
+                        navigate(`/insurance-company`);
+                        setOpened(false);
+                      }
+                      if (message?.is_case_create) {
+                        navigate(`/events`);
+                        setOpened(false);
+                      }
+                    }}
+                    sx={(theme) => ({
+                      backgroundColor:
+                        theme.colorScheme === "dark"
+                          ? theme.colors.dark[6]
+                          : theme.colors.gray[0],
+                      textAlign: "center",
+                      padding: theme.spacing.sm,
+                      borderRadius: theme.radius.md,
+                      marginBottom: theme.spacing.sm,
+                      cursor: "pointer",
+                      "&:hover": {
                         backgroundColor:
                           theme.colorScheme === "dark"
-                            ? theme.colors.dark[6]
-                            : theme.colors.gray[0],
-                        textAlign: "center",
-                        padding: theme.spacing.sm,
-                        borderRadius: theme.radius.md,
-                        marginBottom: theme.spacing.sm,
-                        cursor: "pointer",
-                        "&:hover": {
-                          backgroundColor:
-                            theme.colorScheme === "dark"
-                              ? theme.colors.dark[5]
-                              : theme.colors.gray[1],
-                        },
-                      })}
-                    >
-                      <div>
-                        <Text>
-                          {message?.oao_ie_number &&
-                            message?.appraisal_company_name}
-                          {message?.ie_number && message?.title}
-                          {message?.first_name && message?.first_name + " "}
-                          {message?.second_name && message?.second_name}
-                          {message?.appraisal_company_id &&
-                            !message?.first_name &&
-                            "Insured Event"}
-                        </Text>
-                        <Text size="xs" color="dimmed">
-                          {message.role ||
-                            (message?.ie_number && "insurance_company") ||
-                            (message?.oao_ie_number && "appraisal_company")}
-                          {message?.appraisal_company_id &&
-                            !message?.first_name &&
-                            "New Created"}
-                        </Text>
-                      </div>
-                    </Box>
-                  </React.Fragment>
-                ))}
+                            ? theme.colors.dark[5]
+                            : theme.colors.gray[1],
+                      },
+                    })}
+                  >
+                    <div>
+                      <Text>{message?.user_name}</Text>
+                      <Text size="xs" color="dimmed">
+                        {message?.user_type}
+                      </Text>
+                    </div>
+                  </Box>
+                </React.Fragment>
+              ))}
             </div>
           </ScrollArea>
         ) : (
