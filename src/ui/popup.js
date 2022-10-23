5d90787907fb79116aac60695919f3d88a3ce9a6
100798f <farhodbek.nomonov@mail.ru>
2022-10-23 18:29:39 +0500

    update popup.js

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -1,118 +1,122 @@
-import React, { useState } from 'react'
-import io from 'socket.io-client'
-import axios from 'axios'
-import { Box, Center, Popover, ScrollArea, Text } from '@mantine/core'
-import { NoMessage, Notification } from '../icons'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { _URL } from './../utils'
-import { toast, useToasterStore } from 'react-hot-toast'
-import moment from 'moment'
+import React, { useState } from "react";
+import io from "socket.io-client";
+import axios from "axios";
+import { Box, Center, Popover, ScrollArea, Text } from "@mantine/core";
+import { NoMessage, Notification } from "../icons";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { _URL } from "./../utils";
+import { toast, useToasterStore } from "react-hot-toast";
+import moment from "moment";
+import songById from "../static/notification_sound.mp3";
 
-const socket = io('wss://api.insurextest.link', { reconnect: true })
+const socket = io("wss://api.insurextest.link", { reconnect: true });
+
 function Popup() {
-  const user = useSelector((state) => state.user)
-  const navigate = useNavigate()
-  const [opened, setOpened] = useState(false)
-  const [newMessage, setNewMessage] = useState(false)
-  const [messages, setMessages] = useState([])
-  const { toasts } = useToasterStore()
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const [opened, setOpened] = useState(false);
+  const [newMessage, setNewMessage] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const { toasts } = useToasterStore();
+  let song = new Audio(songById);
 
   React.useInsertionEffect(() => {
     toasts
       .filter((t) => t.visible)
       .filter((_, i) => i >= 1)
-      .forEach((t) => toast.dismiss(t.id))
-  }, [toasts])
+      .forEach((t) => toast.dismiss(t.id));
+  }, [toasts]);
 
   const MessagesFC = (msg = {}) => {
     if (msg.admin_type) {
-      console.log(msg)
-      if (user.role === 'superadmin') {
-        setMessages([...messages, msg])
-        setNewMessage(true)
-        toast.success('New message received')
+      console.log(msg);
+      if (user.role === "superadmin") {
+        setMessages([...messages, msg]);
+        setNewMessage(true);
+        toast.success("New message received");
+        song.play();
       }
       if (
-        user.role === 'insurance_company' &&
+        user.role === "insurance_company" &&
         Number(user.insurance_company.id) === Number(msg.insurance_company_id)
       ) {
-        setMessages(messages.concat([msg]))
-        setNewMessage(true)
-        toast.success('New message received')
+        setMessages(messages.concat(msg));
+        setNewMessage(true);
+        toast.success("New message received");
       }
       if (
-        user.role === 'insurance_company' &&
+        user.role === "insurance_company" &&
         msg?.insurance_companies
-          ?.split(',')
+          ?.split(",")
           ?.includes(`${user?.insurance_company?.id}`)
       ) {
-        setMessages(messages.concat([msg]))
-        setNewMessage(true)
-        toast.success('New message received')
+        setMessages(messages.concat(msg));
+        setNewMessage(true);
+        toast.success("New message received");
       }
       if (
-        user.role === 'appraisal_company' &&
+        user.role === "appraisal_company" &&
         Number(user.appraisal_company.id) === Number(msg.appraisal_company_id)
       ) {
-        setMessages(messages.concat([msg]))
-        setNewMessage(true)
-        toast.success('New message received')
+        setMessages(messages.concat(msg));
+        setNewMessage(true);
+        toast.success("New message received");
       }
     }
-  }
+  };
 
   const MessagesFCS = () => {
     if (user?.auth) {
       axios.get(`${_URL}/push/messages`).then(({ data }) => {
-        if (user?.role === 'superadmin') {
-          setMessages(data?.messages?.filter((res) => res.admin_type))
+        if (user?.role === "superadmin") {
+          setMessages(data?.messages?.filter((res) => res.admin_type));
         }
-        if (user?.role === 'insurance_company') {
+        if (user?.role === "insurance_company") {
           setMessages(
             data?.messages?.filter((res) =>
               Number(res?.insurance_company_id)
                 ? Number(res?.insurance_company_id) ===
                     Number(user?.insurance_company?.id) && res?.admin_type
                 : res?.insurance_company_ids
-                    ?.split(',')
-                    ?.includes(`${user?.insurance_company?.id}`),
-            ),
-          )
+                    ?.split(",")
+                    ?.includes(`${user?.insurance_company?.id}`)
+            )
+          );
         }
-        if (user?.role === 'appraisal_company') {
+        if (user?.role === "appraisal_company") {
           setMessages(
             data?.messages?.filter(
               (res) =>
                 Number(res?.appraisal_company_id) ===
-                  Number(user?.appraisal_company?.id) && res?.admin_type,
-            ),
-          )
+                  Number(user?.appraisal_company?.id) && res?.admin_type
+            )
+          );
         }
-      })
+      });
     }
-  }
+  };
 
   React.useInsertionEffect(() => {
     if (user?.auth) {
-      socket.on('message-send', (msg) => MessagesFC(msg))
+      socket.on("message-send", (msg) => MessagesFC(msg));
     }
-  }, [user?.auth])
+  }, [user?.auth]);
 
   React.useInsertionEffect(() => {
-    MessagesFCS()
-  }, [user?.auth, user?.role])
+    MessagesFCS();
+  }, [user?.auth, user?.role]);
 
   return (
     <Popover
       onClick={() => setNewMessage(false)}
-      className={`${newMessage ? 'popup-notification' : ''}`}
-      style={{ width: 'max-content' }}
+      className={`${newMessage ? "popup-notification" : ""}`}
+      style={{ width: "max-content" }}
       opened={opened}
       onClose={() => setOpened(false)}
       target={
         <Notification
-          style={{ cursor: 'pointer' }}
+          style={{ cursor: "pointer" }}
           onClick={() => setOpened((o) => !o)}
         />
       }
@@ -121,19 +125,19 @@ function Popup() {
     >
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          textAlign: 'center',
-          minWidth: '185px',
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center",
+          minWidth: "185px",
         }}
       >
         {messages?.length > 0 ? (
           <ScrollArea
             style={{
               height: 300,
-              width: 'max-content',
-              minWidth: '185px',
+              width: "max-content",
+              minWidth: "185px",
             }}
             offsetScrollbars
           >
@@ -141,17 +145,17 @@ function Popup() {
               <Box
                 sx={(theme) => ({
                   backgroundColor:
-                    theme.colorScheme === 'dark'
+                    theme.colorScheme === "dark"
                       ? theme.colors.dark[6]
                       : theme.colors.gray[0],
-                  textAlign: 'center',
+                  textAlign: "center",
                   padding: theme.spacing.sm,
                   borderRadius: theme.radius.md,
                   marginBottom: theme.spacing.sm,
-                  cursor: 'pointer',
-                  '&:hover': {
+                  cursor: "pointer",
+                  "&:hover": {
                     backgroundColor:
-                      theme.colorScheme === 'dark'
+                      theme.colorScheme === "dark"
                         ? theme.colors.dark[5]
                         : theme.colors.gray[1],
                   },
@@ -167,65 +171,65 @@ function Popup() {
             {messages
               ?.sort(function (a, b) {
                 return (
-                  moment(b.date_time).format('X') -
-                  moment(a.date_time).format('X')
-                )
+                  moment(b.date_time).format("X") -
+                  moment(a.date_time).format("X")
+                );
               })
               ?.map((message, i) => (
                 <React.Fragment key={message?.id + i}>
                   <Box
                     title={message?.user_type}
                     onClick={(e) => {
-                      e.preventDefault()
-                      e.stopPropagation()
+                      e.preventDefault();
+                      e.stopPropagation();
                       if (message?.is_create_case) {
-                        navigate(`/events`)
-                        setOpened(false)
+                        navigate(`/events`);
+                        setOpened(false);
                       }
-                      if (message?.user_type === 'persons') {
-                        navigate(`/persons#${message?.user_id ?? 'users'}`)
-                        setOpened(false)
+                      if (message?.user_type === "persons") {
+                        navigate(`/persons#${message?.user_id ?? "users"}`);
+                        setOpened(false);
                       }
-                      if (message?.user_type === 'sdp') {
-                        navigate(`/sdp`)
-                        setOpened(false)
+                      if (message?.user_type === "sdp") {
+                        navigate(`/sdp`);
+                        setOpened(false);
                       }
-                      if (message?.user_type === 'agent') {
-                        navigate(`/agents`)
-                        setOpened(false)
+                      if (message?.user_type === "agent") {
+                        navigate(`/agents`);
+                        setOpened(false);
                       }
-                      if (message?.user_type === 'appraiser') {
-                        navigate(`/appraisers`)
-                        setOpened(false)
-                      }
-                      if (
-                        message?.user_type ===
-                        ('appraisal_company' || 'event_create')
-                      ) {
-                        navigate(`/appraiser-company`)
-                        setOpened(false)
+                      if (message?.user_type === "appraiser") {
+                        navigate(`/appraisers`);
+                        setOpened(false);
                       }
                       if (
                         message?.user_type ===
-                        ('insurance_company' || 'event_create')
+                        ("appraisal_company" || "event_create")
                       ) {
-                        navigate(`/insurance-company`)
-                        setOpened(false)
+                        navigate(`/appraiser-company`);
+                        setOpened(false);
+                      }
+                      if (
+                        message?.user_type ===
+                        ("insurance_company" || "event_create")
+                      ) {
+                        navigate(`/insurance-company`);
+                        setOpened(false);
                       }
                     }}
                     sx={(theme) => ({
                       backgroundColor:
-                        theme.colorScheme === 'dark'
+                        theme.colorScheme === "dark"
                           ? theme.colors.dark[6]
                           : theme.colors.gray[0],
-                      textAlign: 'center',
+                      textAlign: "center",
                       padding: theme.spacing.sm,
                       borderRadius: theme.radius.md,
                       marginBottom: theme.spacing.sm,
-                      cursor: 'pointer',
-                      '&:hover': {
+                      cursor: "pointer",
+                      "&:hover": {
                         backgroundColor:
-                          theme.colorScheme === 'dark'
+                          theme.colorScheme === "dark"
                             ? theme.colors.dark[5]
                             : theme.colors.gray[1],
                       },
@@ -250,6 +254,6 @@ function Popup() {
         )}
       </div>
     </Popover>
-  )
+  );
 }
-export default Popup
+export default Popup;
