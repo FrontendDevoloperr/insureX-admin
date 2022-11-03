b1fb3e325f6585d1d95b7727b981c13ae7c21301
100798f <farhodbek.nomonov@mail.ru>
2022-11-03 10:52:07 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import AdminPanel from './admin'
-import Login from './login'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import AdminPanel from "./admin";
+import Login from "./login";
+import { useSelector, useDispatch } from "react-redux";
 import {
   login,
   setRole,
   isInsuranceCompany,
   isAppraisalCompany,
-} from './redux/reducer'
+} from "./redux/reducer";
 
 function App() {
-  console.log('new version GMT +5 23:00 31/10/2022/year')
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const user = useSelector((state) => state.user)
+  console.log("new version GMT +5 23:00 03/10/2022/year");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
   let token = JSON.parse(
-    localStorage.getItem('admin-panel-token-insure-x') ?? '{}',
-  )
+    localStorage.getItem("admin-panel-token-insure-x") ?? "{}"
+  );
 
   React.useEffect(() => {
     const ClearTimeOut = () =>
       setInterval(() => {
-        console.clear()
-      }, 3000)
-    return clearInterval(ClearTimeOut())
-  }, [])
+        console.clear();
+      }, 3000);
+    return clearInterval(ClearTimeOut());
+  }, []);
 
   React.useInsertionEffect(() => {
     if (!user?.auth) {
       if (token?.auth) {
-        dispatch(login(token?.auth))
-        dispatch(setRole(token?.role))
-        dispatch(isInsuranceCompany(token?.insurance_company))
-        dispatch(isAppraisalCompany(token?.appraisal_company))
-      } else navigate('/login')
+        dispatch(login(token?.auth));
+        dispatch(setRole(token?.role));
+        dispatch(isInsuranceCompany(token?.insurance_company));
+        dispatch(isAppraisalCompany(token?.appraisal_company));
+      } else navigate("/login");
     }
-  }, [user?.auth, token])
+  }, [user?.auth, token]);
   return (
     <Routes>
       {user?.auth ? (
@@ -45,7 +45,7 @@ function App() {
         <Route path="/*" element={<Login />} />
       )}
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
